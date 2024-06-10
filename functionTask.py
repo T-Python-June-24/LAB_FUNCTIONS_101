8511656640b505ab24bc7a37e33f6c4d254260c6
D. F <ifonei@hotmail.com>
2024-06-10 14:57:36 +0300

    Adding the documentation of the functions

--- functionTask.py
+++ functionTask.py
@@ -1,6 +1,7 @@
 n = int(input("Enter an integer number: "))
 
 def print_Numbers(n):
+    '''This function takes an integer number n from the user then prints the numbers n to 1 in a triangle shape. It prints n to 1 in the first line then prints n-1 to 1 in the second line, etc.'''
     for i in range(n , 0,-1):
         for j in range (i,0,-1):
             print(j,end=" ")
@@ -8,3 +9,4 @@ def print_Numbers(n):
 
 
 print_Numbers(n)
+print(print_Numbers.__doc__)
