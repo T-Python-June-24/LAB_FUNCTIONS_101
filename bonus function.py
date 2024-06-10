a201b059fe874d56d7b61d2bf021fd074ff572b0
Ghofran <ghofranalsanosi@gmail.com>
2024-06-10 18:34:27 +0300

    lap_function

--- bonus function.py
+++ bonus function.py
@@ -1,14 +1,19 @@
-def pattern(parameter):
+def function(num:int):
     """
     Prints a pattern of decreasing numbers starting from the given integer down to 1.
     """
-    for x in range(parameter, 0, -1):
+    result='-'
+    for x in range(num, 0, -1):
         for y in range(x, 0, -1):
-            print(y, end=" ")
-        print()
+         result += str(y)
+         result += '\n'
+
+        
+        return result
 
 
-
+pattern = function() 
+print(pattern) 
 
 
 
