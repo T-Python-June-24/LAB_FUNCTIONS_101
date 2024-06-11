b1715c0aae759b411db60123831540c0dcc08160
Sarah <monifsarah@gmail.com>
2024-06-11 11:08:41 +0300

    added and print the documentation of the function

--- LAB_FUNCTIONS_101.py
+++ LAB_FUNCTIONS_101.py
@@ -1,9 +1,13 @@
 number=int(input("Enter any postive number : "))
 def tringle_pattern(number):
- 
-     for i in range(number, 0, -1):
+    '''
+    this function  takes parameter of type int that given fron user  , 
+    then it prints out the result formatted in traingle patter
+    '''
+    for i in range(number, 0, -1):
         for j in range(i, 0, -1): # it will print each value in j starts with the i value till 0 
             print(j, end=" ")
         print()
 
 tringle_pattern(number)
+print(tringle_pattern.__doc__)
