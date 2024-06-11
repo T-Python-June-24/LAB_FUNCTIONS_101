05650d001ac536d146422ed007dc1ea7657f8fbe
AZ-007 <aboodi-75-@hotmail.com>
2024-06-11 10:50:11 +0300

    Add  document

--- Program.py
+++ Program.py
@@ -1,8 +1,12 @@
-def pyramidShape(n):
-    for i in range(n, 0, -1):
-        for j in range(i, 0, -1):
+def pyramidShape (n:int ):
+    ''' This function print revers  left pyramid of numbers '''
+
+    for i in range(n,0, -1  ):
+        for j in range( i, 0, -1):
             print(j, end=" ")
-        print()
+    return("")
 
 pyramidShape(5)
+print(pyramidShape.__doc__)
+
 
